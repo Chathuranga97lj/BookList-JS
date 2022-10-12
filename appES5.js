2427e423f3912ee352c9e779486e6a9598284900
Chathuranga97lj <chathuit97@gmail.com>
2022-10-12 12:59:24 +0530

    add book list in inner html

--- appES5.js
+++ appES5.js
@@ -7,7 +7,21 @@ function Book(title, author, isbn) {
 
 // UI 
 function UI() {
+    UI.prototype.addBookToList = function(book) {
+        const list = document.getElementById('book-list');
 
+        // create tr element
+        const row = document.createElement('tr');
+
+        // insert cols
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href="#" class="delete>X</td>
+        `
+        list.appendChild(row);
+    }
 }
 
 // event listners
@@ -16,7 +30,16 @@ document.getElementById('book-form').addEventListener('submit',
         // get form values
         const title = document.getElementById('title').value,
             author = document.getElementById('author').value,
-            isbn = document.getElementById('isbn').value
-        console.log(title, author, isbn);    
+            isbn = document.getElementById('isbn').value    
+
+        // instantiate book
+        const book = new Book(title, author, isbn);
+
+        // instantiate UI
+        const ui = new UI();
+
+        // add book to list
+        ui.addBookToList(book);
+
         e.preventDefault();
     });
